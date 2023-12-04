19babd9d00e5b64bbdfab659f77c279a284b4f1e
aramm <aram.mateos@institutvidreres.cat>
2023-12-04 11:54:26 +0100

    sgdffsg

--- node.js
+++ node.js
@@ -1,15 +1,35 @@
 //NodeJS: localhost:3080
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 port = 3080;
-app.listen(port, () => {
-    console.log(__dirname)
-    console.log(`El servidor escolta per el port:: ${port}`)
-})
+
+server.listen(port, () => {
+    console.log(`Servidor escoltant amb el port:  ${port}`);
+});
+
+
+var videos = [
+    { Video1: 'Cara al Sol Remix' },
+    { Video2: 'Churumbel de Malaga' },
+    { Video3: 'Mandanga Style' },
+    { Video4: 'Varela' }
+];
+
+
+io.on('connection', (socket) => {
+    console.log('Client conectat correctament');
+    socket.emit('listaVideos', videos)
+});
 
 app.use(express.static('Videos'));
 
@@ -27,7 +47,6 @@ app.post("/codiVideo", (req, res) => {
     console.log(generarCodiAleatori())
 })
 
-app.get("/llistaVideos", (req, res) =>{
-    var videos = [{Video1: "Cara al Sol Remix"},{Video2: "Churumbel de Malaga"}, {Video3: "Mandanga Style"}, {Video4: "Varela"}]
-    res.json(videos)
-})
+app.get('/listaVideos', (req, res) => {
+
+});
