32c4c1cc853591785ed9a74154a7b92f4256e984
arnaulloveras <arnau.lloveras@institutvidreres.cat>
2023-12-04 10:44:47 +0100

    Funcio generarCOdiAleatori

--- node.js
+++ node.js
@@ -10,3 +10,19 @@ app.listen(port, () => {
     console.log(__dirname)
     console.log(`El servidor escolta per el port:: ${port}`)
 })
+
+app.use(express.static('Videos'));
+
+function generarCodiAleatori() {
+    const caracters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let codi = '';
+    for (let i = 0; i < 4; i++) {
+        codi += caracters.charAt(Math.floor(Math.random() * caracters.length));
+    }
+    return codi;
+}
+
+app.post("/codiVideo", (req, res) => {
+    generarCodiAleatori();
+    console.log(generarCodiAleatori())
+})
