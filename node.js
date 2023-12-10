eb23ed3775226db97291cfa877dcd1c6a06ea23f
aramm <aram.mateos@institutvidreres.cat>
2023-12-10 19:21:54 +0100

    Tot Funciona Correctament

--- node.js
+++ node.js
@@ -3,10 +3,11 @@ const {Server} = require("socket.io");
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const fs = require('fs');
+
 
 const app = express();
 const server = http.createServer(app);
-//const io = socketIO(server);
 const io = new Server(7777);
 
 const randomstring = require('randomstring');
@@ -41,15 +42,41 @@ function randomCode(){
 
     return codi
 }
-
+var videoData;
+var videoEscollit;
 
 io.on('connection', (socket) => {
     console.log('Client conectat correctament');
     socket.emit('listaVideos', videos)
-    socket.on("verifyCode", (res)=>{
-        console.log(res)
+
+    socket.on('videoEscollit', (res)=>{
+        videoEscollit = res.video
+        console.log(videoEscollit)
     })
     socket.emit('codeFromServer', { code: randomCode() });
+
+    socket.on('sendCode', (res) =>{
+        if (res.toString() === codi){
+            var videoFilePath;
+            if ("Cara al Sol Remix" === videoEscollit){
+                videoFilePath = 'Videos/cara al sol remix.mp4';
+            } else if ("Churumbel de Malaga" === videoEscollit){
+                videoFilePath = 'Videos/Churumbel de Malaga Veniros Todos para la Playa (Video).mp4';
+            } else if("Mandanga Style" === videoEscollit){
+                videoFilePath = 'Videos/Mandanga Style - DJ Theo  Amador.mp4';
+            } else if("Varela" === videoEscollit){
+                videoFilePath = 'Videos/VARELA - YASMINA [VIDEO OFICIAL].mp4';
+            }
+
+            videoData = fs.readFileSync(videoFilePath);
+            console.log("Els codis son iguals, pots veure el video")
+
+        } else {
+            console.log("Els codis nos son iguals.")
+        }
+    })
+    socket.emit('serverResponse', { status: 'correcte', video: videoData });
+
 });
 
 app.use(express.static('Videos'));
